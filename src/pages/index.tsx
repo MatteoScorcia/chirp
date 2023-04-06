7361b71f35a88367c7f2e4516aa8ce89c1697038
MatteoScorcia <matteoscorcia@gmail.com>
2023-04-06 12:28:47 +0200

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,23 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    }
+  });
 
   if (!user) return null;
 
@@ -25,7 +37,18 @@ const CreatePostWizard = () => {
         width={56}
         height={56}
       />
-      <input placeholder="type some emojis!" className="grow bg-transparent outline-none" />
+      <input
+        className="grow bg-transparent outline-none"
+        placeholder="type some emojis!"
+        type="text"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        disabled={isPosting}
+      />
+      <button
+        onClick={() => mutate({ content: input })}>
+        Post
+      </button>
     </div>
   );
 
@@ -71,7 +94,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-      {data?.map(fullPost => (
+      {data.map(fullPost => (
         <PostView key={fullPost.post.id} {...fullPost} />
       ))}
     </div>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,8 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import { type User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -14,7 +15,8 @@ const filterUserForClient = (user: User) => {
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
-      take: 100
+      take: 100,
+      orderBy: [{createdAt: "desc"}]
     });
 
     const users = (await clerkClient.users.getUserList({
@@ -39,5 +41,27 @@ export const postsRouter = createTRPCRouter({
         }
       };
     });
-  })
+  }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      // const { success } = await ratelimit.limit(authorId);
+      // if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
